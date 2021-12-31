6c93eef84e559469cdc4759bffa6372a5c7f59fb
cbpetroff <cp1672a@gmail.com>
2021-12-30 21:41:33 -0800

    Update 20211230
    Update

---
+++ render_website.R
@@ -0,0 +1,15 @@
+render_site(
+  input = ".",
+  output_format = "all",
+  envir = parent.frame(),
+  quiet = FALSE,
+  encoding = "UTF-8"
+)
+
+clean_site(input = ".", preview = TRUE, quiet = FALSE, encoding = "UTF-8")
+
+site_generator(input = ".", output_format = NULL)
+
+site_config(input = ".", encoding = "UTF-8")
+
+default_site_generator(input, output_format_filter = NULL, ...)
